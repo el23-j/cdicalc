b64db0b6a59d0fcc9c1b8ec85ef0629fbff146ff
el23-j <eljabbri98@gmail.com>
2026-03-19 16:27:45 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 const HUQUQPRO_CONFIG = {
   brevo: {
-    apiKey:      "YOUR_BREVO_API_KEY",
-    senderEmail: "noreply@yourdomain.com",
+    apiKey:      "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",
+    senderEmail: "rafael.pullian@gmail.com",
     senderName:  "HuquqPro"
   },
   airtable: {
-    token:   "YOUR_AIRTABLE_PAT",
-    baseId:  "YOUR_BASE_ID",
-    table:   "Leads"
+    token:   "patFw9ptNOzWIBWNo.2f82465b127d6db5d0ddcfcd209f9dd7190c33c22aa407c1326bac1bdae418c1",
+    baseId:  "appQD2qQGMIeSEEMe",
+    table:   "HuquqPro"
   }
 };
